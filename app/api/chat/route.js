274c901000ddbce8640540eb7b7bc957dd527b79
amilcarjose9 <amilcarjpp@gmail.com>
2024-08-23 23:22:07 -0400

    Add basic chat interface

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -44,7 +44,7 @@ export async function POST(req) {
     const openai = new OpenAI()
 
     const text = data[data.length - 1].content
-    const embedding = await OpenAI.Embeddings.create({
+    const embedding = await openai.embeddings.create({
         model: 'text-embedding-3-small',
         input: text,
         encoding_format: 'float',
@@ -80,7 +80,7 @@ export async function POST(req) {
         stream: true,
     })
 
-    const stream = ReadableStream({
+    const stream = new ReadableStream({
         async start(controller){
             const encoder = new TextEncoder()
             try {
